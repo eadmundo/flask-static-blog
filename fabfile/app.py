72eae687fc5b0e85844d206e92ab7a6405c756dc
Edward Stone <edwardjstone@yahoo.com>
2013-01-27 10:14:12 +0000

    clean old files on fab deploy to gh_pages

--- fabfile/app.py
+++ fabfile/app.py
@@ -18,9 +18,19 @@ def serve_frozen():
     do('export FLASK_CONFIG=config/dev.py && %s/bin/python manage.py serve_frozen' % venv_path)
 
 def gh_pages():
+	# switch branch
 	do('git checkout gh-pages')
+	# get rid of everything currently commited to that branch
+	# except .gitignore. this won't get rid of frozen/
+	# as long as it's not been commited by mistake
+	do('git ls-tree gh-pages --name-only |grep -v .gitignore |xargs rm -rf')
+	# copy eveything in frozen/flask-static-blog/ to the top level
 	do('cp -r frozen/flask-static-blog/* .')
+	# get rid of the old copies
 	do('rm -rf frozen/flask-static-blog/')
+	# add, add -u, commit and push
+	do('git add .')
+	do('git add -u')
 	with settings(warn_only=True):
 		do('git commit -am "fab test"')
 	do('git push -f origin gh-pages')
